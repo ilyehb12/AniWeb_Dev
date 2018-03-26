0dca8758d684c6f600927ea993a485b1490508ca
Reinhard <rizajanelafuente@gmail.com>
2018-03-25 17:22:34 -0700

    editted list

--- application/views/AniWebMainSite.php
+++ application/views/AniWebMainSite.php
@@ -122,56 +122,7 @@
                    </div>
         </div>
 
-        <div class="animetile">
-                <div class="animpictile">
-                        <img class="animpic" src="<?php echo base_url(); ?>public/SRC/infinite_stratos.jpg" onclick="watchanime_infinitestratos()">
-                        <p class="animtitle" onclick="watchanime_infinitestratos()">Infinite Stratos</p>
-                   </div>
-                      
-                   <div class="animpictile">
-                        <img class="animpic" src="<?php echo base_url(); ?>public/SRC/log_horizon.jpg" onclick="watchanime_loghorizon()">
-                        <p class="animtitle">Log Horizon</p>
-                   </div>
-                      
-                   <div class="animpictile">
-                         <img class="animpic" src="<?php echo base_url(); ?>public/SRC/made_in_abyss.jpg" onclick="watchanime_madeinabyss()">
-                         <p class="animtitle" onclick="watchanime_loghorizon()">Made in Abyss</p>
-                   </div>
-                     
-                   <div class="animpictile">
-                       <img class="animpic" src="<?php echo base_url(); ?>public/SRC/nogame_nolife.jpg" onclick="watchanime_nogamenolife()">
-                       <p class="animtitle" onclick="watchanime_nogamenolife()">No Game No Life</p>
-                   </div>
-        </div>
-
-        <div class="animetile">
-                <div class="animpictile">
-                        <img class="animpic" src="<?php echo base_url(); ?>public/SRC/rakudai_kishi.jpg" onclick="watchanime_rakudaikishi()">
-                        <p class="animtitle" onclick="watchanime_rakudaikishi()">Rakudai Kishi</p>
-                   </div>
-                      
-                   <div class="animpictile">
-                        <img class="animpic" src="<?php echo base_url(); ?>public/SRC/sao.jpg" onclick="watchanime_sao()">
-                        <p class="animtitle" onclick="watchanime_sao()">S.A.O.</p>
-                   </div>
-                      
-                   <div class="animpictile">
-                         <img class="animpic" src="<?php echo base_url(); ?>public/SRC/tokyo_ghoul.jpg" onclick="watchanime_tokyoghoul()">
-                         <p class="animtitle" onclick="watchanime_tokyoghoul()">Tokyo Ghoul</p>
-                   </div>
-                     
-                   <div class="animpictile">
-                       <img class="animpic" src="<?php echo base_url(); ?>public/SRC/re_zero.jpg" onclick="watchanime_rezero()">
-                       <p class="animtitle" onclick="watchanime_rezero()">Re:Zero</p>
-                   </div>
-        </div>
-
-       
-
-
-
-    </div>
-
+      
     
         <div class="CRtile">
                 <P class="crpara">AniWeb. Copyrights and trademarks for the anime, and other promotional materials are held by their respective owners and their use is allowed under the fair use clause of the Copyright Law.</P>
